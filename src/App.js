8c5cf6c8425df9854f98a6aa5dc36276f3afe963
AbhilashGogoi10 <abhilashgogoi33@gmail.com>
2023-12-22 10:20:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
+import { mockData } from "./mockData";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    const [tab, setTab] = useState(1);
+
+
+ const activeTab = (tabNumber)=>{
+    setTab(tabNumber)
+    }
+
+    const mobiles = mockData.filter((product)=> product.item === "mobiles")
+    const laptops = mockData.filter((product)=> product.item === "laptops")
+    const tv = mockData.filter((product)=> product.item === "tv")
+
+    const data = tab === 1 ? mobiles : tab === 2 ? laptops : tab === 3 ? tv : null;
+
+    return (
+        <div className="App">
+        <div className="category">
+            <div className="mobiles" style={{backgroundColor: tab === 1 ? "yellow" : ""}} onClick={()=> activeTab(1)}>Mobiles</div>
+            <div className="laptops" style={{backgroundColor: tab === 2 ? "yellow" : ""}} onClick={()=> activeTab(2)}>Laptops</div>
+            <div className="tv" style={{backgroundColor: tab === 3 ? "yellow" : ""}} onClick={()=> activeTab(3)}>TV</div>
+        </div>
+
+        <div className="item_div">
+        {data && data.map((val)=>{
+            return(
+                <div>
+            <span>
+                {val.des}
+            </span>
+            </div>
+           )
+
+
+        })}
+        
+        </div>
+
+        </div>
+    )
 }
-
-export default App;
---
+++ src/mockData.js
@@ -0,0 +1,48 @@
+export const mockData = [
+   {
+    id: 1,
+    item: "mobiles",
+    des: "Motorola"
+   },
+   {
+    id: 2,
+    item:"mobiles",
+    des: "Apple",
+   },
+   {
+    id: 3,
+    item: "mobiles",
+    des: "Samsung"
+   },
+ 
+   {
+    id: 4,
+    item: "laptops",
+    des: "Dell"
+   },
+   {
+    id: 5,
+    item: "laptops",
+    des: "Acer"
+   },
+   {
+    id: 6,
+    item: "laptops",
+    des: "MSI"
+   },
+   {
+    id: 7,
+    item: "tv",
+    des: "LG"
+   },
+   {
+    id: 8,
+    item: "tv",
+    des: "Sony"
+   },
+   {
+    id: 9,
+    item: "tv",
+    des: "Hitachi"
+   },
+]
